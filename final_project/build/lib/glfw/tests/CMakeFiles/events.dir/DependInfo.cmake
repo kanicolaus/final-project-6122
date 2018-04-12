e1ad5ee873b48fc9d3b269c3684c79b9a64c480e
Karl Nicolaus <karl@karlnicolaus.com>
2018-04-12 18:06:34 -0400

    working on OSX

---
+++ final_project/build/lib/glfw/tests/CMakeFiles/events.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/karlnico/project-links/final-project-6122/final_project/lib/glfw/deps/getopt.c" "/Users/karlnico/project-links/final-project-6122/final_project/build/lib/glfw/tests/CMakeFiles/events.dir/__/deps/getopt.c.o"
+  "/Users/karlnico/project-links/final-project-6122/final_project/lib/glfw/deps/glad.c" "/Users/karlnico/project-links/final-project-6122/final_project/build/lib/glfw/tests/CMakeFiles/events.dir/__/deps/glad.c.o"
+  "/Users/karlnico/project-links/final-project-6122/final_project/lib/glfw/tests/events.c" "/Users/karlnico/project-links/final-project-6122/final_project/build/lib/glfw/tests/CMakeFiles/events.dir/events.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib/glfw/include"
+  "../lib/glad/include"
+  "../lib/glm"
+  "../lib/glfw/deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/karlnico/project-links/final-project-6122/final_project/build/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- final_project/source/main.cpp
+++ final_project/source/main.cpp
@@ -2,6 +2,7 @@
 #include <Avatar.h>
 #include "../include/Background.h"
 #include "../include/Shader.h"
+#include <GLFW/glfw3.h>
 
 
 //function protptype
@@ -31,8 +32,8 @@ int main() {
     atexit(glfwTerminate);
 
     //this sets up the opengl window by setting the version and all the other necessities
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
